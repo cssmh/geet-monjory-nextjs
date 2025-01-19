a613494da96e72ec42c53cf4242dc2ff37b9d03b
Momen <touristmomen@gmail.com>
2025-01-19 14:45:30 +0600

    moved all to src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ import {heroui} from "@heroui/theme"
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
